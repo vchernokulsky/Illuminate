d5dd7b548c468907ca1e8acb64950ac1f8f566be
sauron <vladimir.chernokulsky@gmail.com>
2013-06-04 09:50:21 +0400

    frame processing tests

--- LightPlayer/src/Tests/FrameProcessorTest.cs
+++ LightPlayer/src/Tests/FrameProcessorTest.cs
@@ -13,7 +13,7 @@ namespace Tests
         private IPackageSender _sender;
         private FrameSequence  _sequence;
 
-        private MockRepository _repository = new MockRepository();
+        private readonly MockRepository _repository = new MockRepository();
 
         [SetUp]
         public void Initialization()
@@ -22,12 +22,18 @@ namespace Tests
             _sender = _repository.StrictMock<IPackageSender>();
         }
 
+        private static Frame CreateFrame(double time1, double time2)
+        {
+            var span1 = TimeSpan.FromSeconds(time1);
+            var span2 = TimeSpan.FromSeconds(time2);
+            var frame = new Frame(span1, span2) { Command = new SetColor(1, Color.FromRgb(128, 128, 128)) };
+            return frame;
+        }
+
         [Test]
         public void ProcessCurrentFrame()
         {
-            var span1 = TimeSpan.FromSeconds(0);
-            var span2 = TimeSpan.FromSeconds(2.5);
-            var frame = new Frame(span1, span2) {Command = new SetColor(1, Color.FromRgb(128, 128, 128))};
+            var frame = CreateFrame(0, 2.5);
             _sequence.Push(frame);
 
             var processor = new FrameProcessor(_sender, _sequence);
@@ -38,7 +44,29 @@ namespace Tests
             }
             using (_repository.Playback())
             {
-                processor.SetTime(TimeSpan.FromSeconds(1.5));
+                processor.SetTime(TimeSpan.FromSeconds(0.1));
+            }
+        }
+
+        [Test]
+        public void ProcessTwoFrames()
+        {
+            var frame1 = CreateFrame(0, 2.5);
+            var frame2 = CreateFrame(2.5, 2);
+            _sequence.Push(frame1);
+            _sequence.Push(frame2);
+
+            var process = new FrameProcessor(_sender, _sequence);
+            using (_repository.Record())
+            {
+                var pkg1 = new Package(frame1.Command.GetBytes());
+                var pkg2 = new Package(frame2.Command.GetBytes());
+                _sender.Expect(x => x.SendPackage(pkg1)).Repeat.Times(2);
+            }
+            using (_repository.Playback())
+            {
+                process.SetTime(TimeSpan.FromSeconds(0.1));
+                process.SetTime(TimeSpan.FromSeconds(2.6));
             }
         }
     }
