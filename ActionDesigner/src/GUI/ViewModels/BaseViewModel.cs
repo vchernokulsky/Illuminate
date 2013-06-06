28db69380fb61e790b6af03c6aaa36ff16be2651
sauron <vladimir.chernokulsky@gmail.com>
2013-06-06 17:36:56 +0400

    start develop UI for light designer

---
+++ ActionDesigner/src/GUI/ViewModels/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Intems.LightDesigner.GUI.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public virtual event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propName)
+        {
+            var pc = PropertyChanged;
+            if (pc != null)
+                pc(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
---
+++ ActionDesigner/src/GUI/ViewModels/FrameListModel.cs
@@ -0,0 +1,19 @@
+﻿using Intems.LightPlayer.BL;
+
+namespace Intems.LightDesigner.GUI.ViewModels
+{
+    public class FrameListModel : BaseViewModel
+    {
+        private FrameSequence _frames;
+
+        public FrameSequence Frames
+        {
+            get { return _frames; }
+            set
+            {
+                _frames = value;
+                RaisePropertyChanged("FrameListModel");
+            }
+        }
+    }
+}
