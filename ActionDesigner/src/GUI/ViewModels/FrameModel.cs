d4a1b273c81d02c4bbd042674e3aeefa8de387bf
sauron <vladimir.chernokulsky@gmail.com>
2013-06-07 14:07:27 +0400

    view models

---
+++ ActionDesigner/src/GUI/ViewModels/FrameModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace Intems.LightDesigner.GUI.ViewModels
+{
+    public class FrameModel :BaseViewModel
+    {
+        public Color Color { get; set; }
+
+        public string CmdType { get; set; }
+
+        // Time
+        public TimeSpan FrameBegin { get; set; }
+
+        public TimeSpan FrameLength { get; set; }
+    }
+}
