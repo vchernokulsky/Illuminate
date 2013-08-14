317a148c0a88bd0d6b4525fe144a8281dc4fbc98
sauron <vladimir.chernokulsky@gmail.com>
2013-08-14 19:05:42 +0400

    work with: #7

--- LightPlayer/src/Transport/UdpSender.cs
+++ LightPlayer/src/Transport/UdpSender.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Intems.LightPlayer.Transport
 {
     class UdpSender : IPackageSender
     {
+        private readonly Socket _socket;
+        private readonly IPEndPoint _endPoint;
+
+        public UdpSender(string ipAddr, string port)
+        {
+            var remoteAddr = IPAddress.Parse(ipAddr);
+            var remotePort = Int32.Parse(port);
+            _endPoint = new IPEndPoint(remoteAddr, remotePort);
+
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        }
+
         public void SendPackage(Package package)
         {
-            throw new NotImplementedException();
+            if (_socket != null)
+            {
+                try
+                {
+                    _socket.SendTo(package.ToArray(), _endPoint);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
         }
     }
 }
