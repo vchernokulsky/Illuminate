06c47802d24e7a36b2a0182442fa1bbdccf504c6
sauron <vladimir.chernokulsky@gmail.com>
2014-02-10 21:30:18 +0400

    broadcast

---
+++ LightPlayer/src/Transport/BroadcastSender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Intems.LightPlayer.Transport
+{
+    public class BroadcastSender : IPackageSender
+    {
+        public void SendPackage(Package package)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendPackages(IEnumerable<Package> packages)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
