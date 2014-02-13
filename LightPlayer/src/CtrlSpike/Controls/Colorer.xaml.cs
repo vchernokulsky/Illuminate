07dc57bbe4ca66626b8f69c2ced721d346373dac
sauron <vladimir.chernokulsky@gmail.com>
2014-02-13 17:42:19 +0400

    new color picker

---
+++ LightPlayer/src/CtrlSpike/Controls/Colorer.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+using Button = System.Windows.Controls.Button;
+using UserControl = System.Windows.Controls.UserControl;
+
+namespace CtrlSpike.Controls
+{
+    /// <summary>
+    /// Interaction logic for ColorSelect.xaml
+    /// </summary>
+    public partial class Colorer : UserControl
+    {
+        public Colorer()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSelectButtonClick(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn != null)
+            {
+                var dlg = new ColorDialog();
+                dlg.ShowDialog();
+            }
+        }
+    }
+}
