4522e21afe35371cbb1741e4f4bd1ab0efdf85ef
sauron <vladimir.chernokulsky@gmail.com>
2014-04-11 12:13:13 +0400

    #13 milliseconds in frame UI

--- LightPlayer/src/Editor/Converters/TimeSpanToShortTime.cs
+++ LightPlayer/src/Editor/Converters/TimeSpanToShortTime.cs
@@ -11,18 +11,20 @@ namespace Intems.LightDesigner.GUI.Converters
             var val = (TimeSpan) value;
             var sMin = String.Format("{00:0}", val.Minutes);
             var sSec = String.Format("{00:0}", val.Seconds);
+            var sMilSec = val.Milliseconds > 0 ? String.Format(",{0:0}", val.Milliseconds/100) : String.Empty;
+
             sMin = sMin.Length < 2 ? "0" + sMin : sMin;
             sSec = sSec.Length < 2 ? "0" + sSec : sSec;
 
-            return String.Format("{0}:{1}", sMin, sSec);
+            return String.Format("{0}:{1}{2}", sMin, sSec, sMilSec);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var val = (string)value;
             var str = val.Split(':');
-            var min = double.Parse(str[0]);
-            var sec = double.Parse(str[1]);
+            var min = Double.Parse(str[0]);
+            var sec = Double.Parse(str[1], NumberStyles.Float);
 
             return TimeSpan.FromSeconds(min*60 + sec);
         }
